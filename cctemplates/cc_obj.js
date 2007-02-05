3bb6796cb97610d4ebde460960132921c982c42d
fourstones <fourstones@committer.creativecommons.org>
2007-02-05 23:41:31 +0000

    4.0 mondo checkin (cctemplates)

--- cctemplates/cc_obj.js
+++ cctemplates/cc_obj.js
@@ -22,23 +22,9 @@ var cc_target;
 
 function cc_obj(name)
 {
-	if (document.getElementById)
-	{
-	  this.obj = document.getElementById(name);
-	  if( !this.obj )
-		alert("no obj: " + name);
-	  this.style = document.getElementById(name).style;
-	}
-	else if (document.all)
-	{
-	  this.obj = document.all[name];
-	  this.style = document.all[name].style;
-	}
-	else if (document.layers)
-	{
-	  this.obj = document.layers[name];
-	  this.style = document.layers[name];
-	}
+    var obj = $(name);
+    obj.obj =  obj; // tada!
+    return obj;
 }
 
 function cc_set_text(obj,text)
@@ -55,73 +41,24 @@ function cc_set_text(obj,text)
 //just ping ccHost code
 function cc_ping_cchost(url)
 {
-    if (window.XMLHttpRequest)
-    { 
-        cc_xml = new XMLHttpRequest();            //set the request
-    }
-    else if(window.ActiveXObject)
-    { 
-        //Create our RequestObject
-        cc_xml = new ActiveXObject("Microsoft.XMLHTTP"); 
-    }
-
-    if( cc_xml )
-    {
-        cc_xml.open("GET", url, true);            //set the page to request
-        cc_xml.send(null);                        //initialize the request 
-    }
+    new Ajax.Request( url, { method: 'get' } );
 }
 
 //Create our function to start the request
 function cc_get_data(url,element_name,default_msg)
 {
-    //inform there is an action!
-    iobj = new cc_obj(element_name);
-    iobj.obj.innerHTML = default_msg;
-
-    cc_target = element_name;
-
-    //determine if the browser is Moz, FF, NN, Op
-    if (window.XMLHttpRequest)
-    { 
-        cc_xml = new XMLHttpRequest();            //set the request
-        cc_xml.onreadystatechange = cc_response;  //function to call on each set
-        cc_xml.open("GET", url, true);            //set the page to request
-        cc_xml.send(null);                        //initialize the request 
-    }
-    // IE
-    else if(window.ActiveXObject)
-    { 
-        //Create our RequestObject
-        cc_xml = new ActiveXObject("Microsoft.XMLHTTP"); 
-        if(cc_xml)
-        { 
-            cc_xml.onreadystatechange = cc_response;  //function to call on each step
-            cc_xml.open("GET", url, true);            //set the page to request
-            cc_xml.send(null);                        //initialize the request
-        }
-    }
-    else
-    {   // er, this'll be lovely
-        document.write("<iframe src=\"" + url + "\" style=\"border:0px;width:80%;\"></iframe>");
-    }       
+    $(element_name).innerHTML = default_msg;
+    new Ajax.Updater ( element_name, url, { method: 'get' } );
 }
-          
-function cc_response()
-{
-    //Look to see if the request is in the 4th stage (complete)         
-    if(cc_xml.readyState == 4)
-    {
-        //Make sure that we get a sucess page status   
-        target = new cc_obj(cc_target);
 
-        if(cc_xml.status == 200)
-        {
-            target.obj.innerHTML = cc_xml.responseText;
-        }
-        else
-        {
-            target.obj.innerHTML = "err, no - Status: " + cc_xml.status;
-        }
-    }
+function cc_add_tag(tag,fieldname)
+{
+  f = new cc_obj(fieldname);  
+  tagstr = f.obj.value;
+  tags = tagstr.split(',');
+  tags.push(tag);
+  tagstr = tags.join(',');
+  if( tagstr.charAt(0) == ',' )
+    tagstr = tagstr.substr(1);
+  f.obj.value = tagstr;
 }
--- cctemplates/dl_popup.js
+++ cctemplates/dl_popup.js
@@ -1,59 +1,40 @@
+/* cc javascript */
+
+
 var cc_current_dl_box;
 var cc_in_open;
 
 function cc_close_dl_box()
 {
     if( cc_in_open )
-    {
       cc_in_open = false;
-    }
     else if( cc_current_dl_box )
-    {
       cc_current_dl_box.style.display = "none";
-    }
 }
 
-document.body.onclick = cc_close_dl_box;
+function cc_dl_winclick()
+{
+    cc_close_dl_box();
+}
+
+Event.observe( document.body, 'click', cc_dl_winclick );
 
 function cc_show_dl_box(event,upload_id,button)
 {
-    obj = new cc_obj('dlmenu' + upload_id);
+    obj = $('dlmenu' + upload_id);
 
     if( (!obj.style.display) || (obj.style.display == "none") )
     {
+        if( typeof button == 'undefined' )
+            button = Event.element(event); // hope this works...
+
         cc_close_dl_box();
-       
-        x = 0;
-        y = 0;
-        if (!event) 
-          event = window.event;
 
-        if( document.all && obj.obj.offsetLeft )
-        {
-            coords = cc_get_ie_pos(obj.obj);
-            x = coords.left; // + 55;
-            y = coords.top; //  + 55;
-        }
-        else if (event.pageX || event.pageY)
-        {
-            x = event.pageX + 5;
-            y = event.pageY + 5;
-        }
-        else if (event.clientX || event.clientY)
-        {
-            x = event.clientX + document.body.scrollLeft + 5;
-            y = event.clientY + document.body.scrollTop + 5;
-        }
-
-        // IE likes it this way
-        obj.style.top  = y;
-        obj.style.left = x;
-        obj.style.display = "block";
-        obj.style.position = "absolute";
-
-        // Moz likes it this way
-        val = "position:absolute;display:block;left:" + x + "px;top:" + y + "px;";
-        obj.obj.setAttribute('style',val);
+        Position.clone( button, obj, {  setWidth:   false,
+                                        setHeight:  false,
+                                        offsetTop:  20,
+                                        offsetLeft: 20 } );
+        Element.show(obj);
 
         cc_current_dl_box = obj;           
         cc_in_open = true;
@@ -65,33 +46,13 @@ function cc_show_dl_box(event,upload_id,button)
     return(false);
 }
 
-function cc_get_ie_pos (obj) 
-{
-  var left = obj.offsetLeft;
-  var top  = obj.offsetTop;
-  if( left < 0 )
-  {
-    while ((obj = obj.offsetParent) != null) 
-    {
-      left += obj.offsetLeft; 
-      top  += obj.offsetTop;
-    }
-
-    left += 55; // slightly more than half the width
-    top += 55;
-  }
-  var retobj = new Object();
-  retobj.left = left;
-  retobj.top  = top;
-  return(retobj);
-}
 
 function cc_star_hover_on(id,star_num)
 {
     for( i = 1; i < (star_num+1); i++ )
     {
         var name = 'cc_star_' + id + '_' + i;
-        cc_set_img( name, root_url + 'ccimages/stars/star-red.gif' );
+        $( name ).src = root_url + 'ccimages/stars/star-red.gif';
     }
     
 }
@@ -101,7 +62,7 @@ function cc_star_hover_off(id,star_num)
     for( i = 1; i < 6; i++ )
     {
         var name = 'cc_star_' + id + '_' + i;
-        cc_set_img( name, root_url + 'ccimages/stars/star-empty.gif' );
+        $( name ).src = root_url + 'ccimages/stars/star-empty.gif';
     }
 
 }
@@ -109,20 +70,14 @@ function cc_star_hover_off(id,star_num)
 function cc_star_pick(id,star_num)
 {
     var hname = "cc_rate_head_" + id;
-    obj = new cc_obj(hname);
-    obj.style.display = "inline";
+    $(hname).style.display = "inline";
 
     var bname = "cc_star_block_" + id;
-    obj = new cc_obj(bname);
-    obj.style.display = "none";
+    $(bname).style.display = "none";
 
     var dname = "cc_rate_block_" + id;
+    if( typeof lang_get_ratings == 'undefined' )
+        lang_get_ratings = '...';
     cc_get_data(home_url + "rate/" + id + "/" + star_num,dname,lang_get_ratings);
 }
 
-function cc_set_img(name,img)
-{
-    obj = new cc_obj( name );
-    if( obj.obj.src )
-       obj.obj.src = img;
-}
