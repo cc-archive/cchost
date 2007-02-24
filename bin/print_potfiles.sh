9a75bf96f0c102fc7064b2b393ba919f0daf6921
kidproto <kidproto@committer.creativecommons.org>
2007-02-24 07:13:58 +0000

    Added more sane way to get which files have _()/gettext strings in them...

--- bin/print_potfiles.sh
+++ bin/print_potfiles.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-find . -name "*.php" -o -name "*.inc" | xargs grep -l "_([^\'\"].*[^\'\"])" | sort
+#
+# This script looks to find files which use gettext to do language calls. 
+# The recommended usage is to run this script and redirect to a file like
+# POTFILES.in for then editing to make sure you have the files you want.
+#
+
+echo '# List of source files containing translatable strings.
+# Please keep this file sorted alphabetically.
+# If majorly out of order, use bin/print_potfiles.sh to regenerate
+[encoding: UTF-8]'
+
+find . -name "*.php" -o -name "*.inc" | xargs grep -l "_(.*)" | sort | sed -e 's/\.\///'
+
+# TO ADD ' \' onto end of lines, add -e 's/$/ \\/'
+
