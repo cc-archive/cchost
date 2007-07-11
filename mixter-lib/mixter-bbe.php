bb619ed7b0c19f8e3f9a1e5f7c0ec7d4fe7d9828
fourstones <fourstones@committer.creativecommons.org>
2007-07-11 08:59:55 +0000

    dynamic text reading

--- mixter-lib/mixter-bbe.php
+++ mixter-lib/mixter-bbe.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 
-function bbe_main()
+if( !empty($_GET['bbe_format']) )
 {
     require_once('../cclib/snoopy/Snoopy.class.php');
     $snoopy = new Snoopy();
@@ -11,7 +11,40 @@ function bbe_main()
     exit;
 }
 
-if( !empty($_GET['bbe_format']) )
-    bbe_main();
+
+if( !empty($_GET['bbe_edit']) )
+{
+    $f1 = @file_get_contents('mixter-files/bbe_1.txt');
+    $f2 = @file_get_contents('mixter-files/bbe_2.txt');
+    $html =<<<EOF
+    <style>
+        textarea {
+            width: 80%;
+            height: 200px;
+            margin: 6px;
+            font-family: verdana;
+            font-size: 11px;
+        }
+    </style>
+    <form action="/mixter-lib/mixter-bbe.php?bbe_post=1" method="post">
+    <textarea name="bbe_1">$f1</textarea><br />
+    <textarea name="bbe_2">$f2</textarea><br />
+    <input type="submit" value="submit changes" />
+    </form>
+EOF;
+    print $html;
+    exit;
+}
+
+if( !empty($_REQUEST['bbe_post']) )
+{
+ error_reporting(E_ALL); 
+   $text = $_POST['bbe_1'];
+    $f = fopen('../mixter-files/bbe_1.txt','w'); fwrite($f,$text); fclose($f);
+    $text = $_POST['bbe_2'];
+    $f = fopen('../mixter-files/bbe_2.txt','w'); fwrite($f,$text); fclose($f);
+    header("Location: /bbe");
+    exit;
+}
 
 ?>
