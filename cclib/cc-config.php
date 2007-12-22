591c830d55e109f3db3045c6a7a7a269e752fdf7
fourstones <fourstones@committer.creativecommons.org>
2007-12-22 02:36:08 +0000

    quick work around

--- cclib/cc-config.php
+++ cclib/cc-config.php
@@ -363,7 +363,7 @@ class CCConfigs extends CCTable
             if( !empty($tags) )
             {
                 $tags = unserialize($tags);
-                $title = $tags['site-title'];
+                $title = 'cchost site'; // todo: where's that?? $tags['site-title'];
             }
             else
             {
@@ -449,7 +449,7 @@ function cc_check_site_enabled()
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-    <title>{$CC_GLOBALS['site-title']}</title>
+    <title>cchost site</title>
     $css_link
 </head>
 <body>
