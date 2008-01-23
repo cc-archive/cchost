ea9e233144110663b88b47e5bf9a55accfa88d75
fourstones <fourstones@committer.creativecommons.org>
2008-01-23 02:55:37 +0000

    more ie6 cleanup

--- ccskins/shared/formats/upload_page_wide.php
+++ ccskins/shared/formats/upload_page_wide.php
@@ -39,7 +39,7 @@ div#upload_menu_box{float:left;width:20%;margin-left:-100%;padding-left:1.5em;}
 div#upload_wrapper{float:left;width:100%;}
 div#upload_middle{margin: 0 35% 0 23%;}
 div#upload_sidebar_box{float:left;width:30%;margin-left:-30%;}
-div#upload_menu_box{float:left;width:20%;margin-left:-85%;}
+div#upload_menu_box{float:left;width:24%;margin-left:-85%;}
 </style>
 <![endif]-->
 
--- ccskins/shared/layouts/box_round.php
+++ ccskins/shared/layouts/box_round.php
@@ -8,24 +8,19 @@
 */
 
 
-function _t_box_round_invoke_script($A,$T)
-{
-    ?><script type="text/javascript">cc_round_boxes();</script><?
-}
-
-$A['end_script_blocks'][] = __FILE__ . '/invoke_script';
+$A['end_script_text'][] = 'cc_round_boxes();';
 
 ?>
 
 <script type="text/javascript" src="<?= $T->URL('js/round-box.js'); ?>"></script>
 <link rel="stylesheet" type="text/css" href="<?= $T->URL('layouts/box_round.css'); ?>" title="Default Style" />
-<!--[if gt IE 6.]> 
+<!--[if IE]> 
 <link rel="stylesheet" type="text/css" href="<?= $T->URL('layouts/box_round_ie.css'); ?>" title="Default Style" />
 <![endif]-->
 
 <style type="text/css">
 .cssbox, .cssbox_body, .cssbox_head, .cssbox_head h2
-{ background-image: url('<?= $T->URL('images/backbox-mono.png') ?>') } 
+{ background-image: url('<?= $T->URL('images/backbox-mono.gif') ?>') } 
 </style>
 
 
@@ -33,15 +28,3 @@ $A['end_script_blocks'][] = __FILE__ . '/invoke_script';
 var round_box_enabled = 1;
 </script>
 
-<!--[if lt IE 7.]>
-<? require_once( dirname(__FILE__) . '/box_square.php' ); ?>
-<style>
-.box {
-    overflow: hidden;
-}
-</style>
-<script type="text/javascript">
-var round_box_enabled = null;
-var disable_round_box = true;
-</script>
-<![endif]-->
