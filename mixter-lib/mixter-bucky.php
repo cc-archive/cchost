d2a8dfa423fb024ccb61a9ebb56b506b8d58d522
fourstones <fourstones@committer.creativecommons.org>
2008-01-29 10:03:42 +0000

    moved cclib

--- mixter-lib/mixter-bucky.php
+++ mixter-lib/mixter-bucky.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 
 if( !empty($_GET['bucky_format']) )
 {
-    require_once('../cclib/snoopy/Snoopy.class.php');
+    require_once('../cchost_lib/snoopy/Snoopy.class.php');
     $snoopy = new Snoopy();
     $snoopy->fetch('http://ccmixtermedia.org/buckyjonson');
     print $snoopy->results;
