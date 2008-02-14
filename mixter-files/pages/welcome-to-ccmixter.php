d3ffd67ed2b4975a5e2e447f5f0674ea49ae2326
fourstones <fourstones@committer.creativecommons.org>
2008-02-14 15:28:01 +0000

    bad url in topic query

--- mixter-files/pages/welcome-to-ccmixter.php
+++ mixter-files/pages/welcome-to-ccmixter.php
@@ -19,7 +19,7 @@ $A['content_page_box'] = '1';
 $A['content_page_paging'] = '1';
 $A['content_page_textformat'] = 'format';
 $A['content_page_columns'] = '2';
-$A['topic_url'] = 'http://cchost.org/api/query?datasource=topics&t=ccskins/shared/pages/content_page.tpl&ids=';
+$A['topic_url'] = 'http://ccmixter.org/api/query?datasource=topics&t=ccskins/shared/pages/content_page.tpl&ids=';
 print "<h1>Welcome to ccMixter</h1>";
  cc_query_fmt('datasource=topics&f=page&t=ccskins/shared/pages/content_page.tpl&sort=date&ord=desc&type=home&limit=&ids=');
  $A['macro_names'][] = 'prev_next_links';
