bbb25ece03f7c7f009181b549378e2764466428e
fourstones <fourstones@committer.creativecommons.org>
2008-02-14 15:46:08 +0000

    clean up pool item listing

--- ccdataviews/pool_item_list.php
+++ ccdataviews/pool_item_list.php
@@ -19,9 +19,9 @@ function pool_item_list_dataview()
         pool_name, pool_short_name, pool_description, pool_site_url,
         CONCAT( '$urli', pool_item_id) as pool_item_page,
         CONCAT( '$urlp', pool_id ) as pool_url,
-        CONCAT( '$urll', license_logo ) as license_logo_url, license_url,
-        CONCAT( '$urlls', license_logo ) as license_logo_url_small,
-        license_name, pool_item_extra
+        IF( pool_short_name = '_web', '', CONCAT( '$urll', license_logo ) )  as license_logo_url, 
+        IF( pool_short_name = '_web', '', CONCAT( '$urlls', license_logo ) ) as license_logo_url_small,
+        license_url, license_name, pool_item_extra
     FROM cc_tbl_pool_item
     JOIN cc_tbl_pools ON pool_item_pool = pool_id
     JOIN cc_tbl_licenses ON pool_item_license = license_id
