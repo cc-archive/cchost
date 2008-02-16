69ddd044bb37de709c2dad17bbe74e89dc50f00f
fourstones <fourstones@committer.creativecommons.org>
2008-02-16 06:17:42 +0000

    cleaned up trackback admin'ing

--- cchost_lib/cc-pools-ui.php
+++ cchost_lib/cc-pools-ui.php
@@ -144,60 +144,92 @@ END;
         CCPage::PageArg('client_menu',$args,'print_client_menu');
     }
 
-    function _process_pool_items($actions)
+    function _delete_trackback($pool_item_id)
     {
-        $approved = array();
-        $upload_ids = array();
-        foreach( $actions as $id => $action )
-        {
-            if( $action == 'nothing' )
-                continue;
-            $id = CCUtil::StripText($id);
-            $uids = CCDatabase::QueryItems("SELECT pool_tree_parent FROM cc_tbl_pool_tree WHERE pool_tree_pool_child = $id");
-            $upload_ids = array_merge( $upload_ids, $uids );
+        // For delete if the item has never been approved then it's just a matter of 
+        // deleting the pool_item with the trackback info. However if the admin is
+        // deleting an approved trackback, then we remove the pool_item, clear the tree 
+        // and re-synch but will NOT remove the ttype-tags from any associated uploads.
+        // This must be manually by the admins. (it is simply too much bookkeeping to
+        // track all the ttype-tags that might or might not be relevant)
+        //
+        // So the lesson is: When you approve something make sure you really mean to
+        //
+        $row = CCDatabase::QueryRow('SELECT * FROM cc_tbl_pool_item WHERE pool_item_id='.$pool_item_id);
+        if( empty($row) )
+            return;
+        
+        CCDatabase::Query('DELETE FROM cc_tbl_pool_item WHERE pool_item_id='.$pool_item_id);
+        $id = $pool_item_id;
+        $uids = CCDatabase::QueryItems("SELECT pool_tree_parent FROM cc_tbl_pool_tree WHERE (pool_tree_pool_parent = $id) OR " 
+                                        . "(pool_tree_pool_child = $id)");
 
-            if( $action == 'delete' )
-            {
-                CCDatabase::Query('DELETE FROM cc_tbl_pool_item WHERE pool_item_id = '.$id);
-                CCDatabase::Query("DELETE FROM cc_tbl_pool_tree WHERE (pool_tree_pool_parent = $id) && (pool_tree_pool_child = $id)");
-            }
-            else
-            {
-                $approved[] = $id;
-            }
-        }
-
-        if( !empty($approved) )
+        $uids = array_unique( array_filter($uids) );
+        if( !empty($uids) )
         {
-            $approved = join(',',$approved);
-            $sql = "UPDATE cc_tbl_pool_item SET pool_item_approved = 1, pool_item_num_sources =  (pool_item_num_sources+1) WHERE pool_item_id IN ({$approved})";
-            CCDatabase::Query($sql);
-        }
-        $upload_ids = array_filter(array_unique($upload_ids));
-        require_once('cchost_lib/cc-sync.php');
-        foreach( $upload_ids as $upload_id )
-            CCSync::Upload($upload_id);
-        if( !empty($approved) )
-        {
-            $rows = CCDatabase::QueryRows("SELECT pool_item_extra FROM cc_tbl_pool_item WHERE pool_item_id IN ({$approved})");
-            require_once('cchost_lib/cc-uploadapi.php');
-            foreach( $rows as $row )
+            CCDatabase::Query("DELETE FROM cc_tbl_pool_tree WHERE (pool_tree_pool_parent = $id) OR (pool_tree_pool_child = $id)");
+            require_once('cchost_lib/cc-sync.php');
+            foreach( $uids as $upload_id )
             {
-                if( empty($row['pool_item_extra']) )
-                    continue;
-                $ex = unserialize($row['pool_item_extra']);
-                if( empty($ex['ttype']) )
-                    continue;
-                $upload_id = $ex['upload_id'];
-                CCUploadAPI::UpdateCCUD($upload_id,'in_' . $ex['ttype'] . ',trackback','');
+                CCSync::Upload($upload_id);
             }
         }
     }
 
-    function ItemDelete($pool_item)
+    function ApproveTrackback($pool_item_id,$upload_id=0)
     {
-        $actions[$pool_item] = 'delete';
-        $this->_process_pool_items($actions);
+        //
+        // for approval the trackbacks are pool items with the pool_item_approved set to 0
+        // and inside pool_item_extra['upload_id'] is a comma separated list of upload_id's 
+        // to attach the trackback to. This is done by createing a record in the
+        // pool_tree table
+        //
+        // during approve we create tags on the upload based on pool_item_extra['ttype']
+        //
+        // The upload needs to be synch'd on num_pool remixes
+        // 
+
+        $row = CCDatabase::QueryRow('SELECT * FROM cc_tbl_pool_item WHERE pool_item_id='.$pool_item_id);
+        $sql = "UPDATE cc_tbl_pool_item SET pool_item_approved = 1, pool_item_num_sources =  (pool_item_num_sources+1) WHERE pool_item_id="
+                     . $pool_item_id;
+
+        CCDatabase::Query($sql);
+
+        if( empty($upload_id) )
+        {
+            $ex = empty($row['pool_item_extra']) ? array() : unserialize($row['pool_item_extra']);
+            if( empty($ex['ttype']) )
+                return;
+            $need_tags = split(',',$ex['upload_id']);
+            if( empty($need_tags) )
+                return;
+        }
+        else
+        {
+            $need_tags = array($upload_id);
+        }
+
+        require_once('cchost_lib/cc-sync.php');
+        require_once('cchost_lib/cc-uploadapi.php');
+
+        $pool_tree = new CCPoolTree();
+        foreach( $need_tags as $upload_id )
+        {
+            $x['pool_tree_parent'] = $upload_id;
+            $x['pool_tree_pool_child'] = $pool_item_id;
+            $row = $pool_tree->QueryRow($x);
+            if( empty($row) )
+                $pool_tree->Insert($x);
+
+            CCUploadAPI::UpdateCCUD($upload_id,'trackback,in_' . $ex['ttype'] ,'');
+            CCSync::Upload($upload_id);
+        }
+    }
+
+
+    function ItemDelete($pool_item_id)
+    {
+        $this->_delete_trackback($pool_item_id);
         CCUtil::ReturnAjaxMessage(_('Pool item deleted'));
     }
 
@@ -205,20 +237,88 @@ END;
     function Approve($submit='')
     {
         require_once('cchost_lib/cc-page.php');
-        CCPage::SetTitle(_("Approve Pending Remixes"));
+        CCPage::SetTitle(_("Approve Pending Trackbacks"));
         if( $submit )
         {
-            $this->_process_pool_items($_POST['action']);
+            // this is called when admin wants to 1) approve, 2) delete or 3) noop a trackback
+
+            $approved = array();
+            $upload_ids = array();
+            foreach( $_POST['action'] as $id => $action )
+            {
+                if( $action == 'nothing' )
+                    continue;
+                $id = CCUtil::StripText($id);
+                if( $action == 'delete' )
+                    $this->_delete_trackback($id);
+                elseif( $action == 'approve' )
+                    $this->ApproveTrackback($id);
+            }
         }
 
-        require_once('cchost_lib/cc-query.php');
-        $query = new CCQuery();
-        $args = $query->ProcessAdminArgs('t=pool_approvals&datasource=pool_items');
-        $query->Query($args);
+        $pool_id = $this->GetWebSamplePool();
+        $sql =<<<EOF
+        SELECT pool_item_id, pool_item_url, pool_item_name, pool_item_download_url,
+               pool_item_extra, pool_name, pool_item_artist
+        FROM cc_tbl_pool_item
+        JOIN cc_tbl_pools     ON pool_item_pool       = pool_id
+        WHERE (pool_item_approved = 0) and pool_item_pool = {$pool_id}
+EOF;
+
+        $records = CCDatabase::QueryRows($sql);
+        $ccl = ccl('files') . '/';
+        $k = array_keys($records);
+        $c = count($k);
+        for( $i = 0; $i < $c; $i++ )
+        {
+            $R =& $records[$k[$i]];
+            $R['pool_item_extra'] = unserialize($R['pool_item_extra']);
+            if( !empty($R['pool_item_extra']['upload_id']) )
+            {
+                $sql =<<<EOF
+                SELECT  upload_name,user_real_name,user_name,
+                        CONCAT( '{$ccl}', user_name, '/', upload_id ) as file_page_url
+                FROM cc_tbl_uploads   
+                JOIN cc_tbl_user      ON upload_user          = user_id
+                WHERE upload_id IN ({$R['pool_item_extra']['upload_id']})
+EOF;
+                $R['uploads'] = CCDatabase::QueryRows($sql);
+            }
+        }
+//d($records);
+        CCPage::PageArg('records',$records,'pool_approvals');
+    }
+
+    function GetWebSamplePool()
+    {
+        $pool_id = CCDatabase::QueryItem('SELECT pool_id FROM cc_tbl_pools WHERE pool_short_name = \'_web\'');
+        if( empty($pool_id) )
+        {
+            require_once('cchost_lib/cc-pools.php');
+            $pools = new CCPools();
+            $a['pool_id'] = $pools->NextID();
+            $a['pool_name'] = _('Trackback Sitings');
+            $a['pool_short_name'] = '_web';
+            $a['pool_description'] = _('People link to us!');
+            // pool_api_url can ba a local module for searching the pool
+            // classname:module_path
+            // CCMagnatune:mixter-lib/mixter-magnatune.inc
+            $a['pool_api_url'] = '';
+            $a['pool_site_url'] = ccl();
+            $a['pool_ip'] = '255.0.0.0';
+            $a['pool_banned'] = 0;
+            $a['pool_search'] = 0;
+            $a['pool_default_license'] = '';
+            $a['pool_auto_approve'] = 0;
+            $pools->Insert($a);
+            $pool_id = $a['pool_id'];
+        }
+        return $pool_id;
     }
 
     function Manage()
     {
+        require_once('cchost_lib/cc-page.php');
         CCPage::SetTitle(_("Manage Sample Pools"));
 
         $pools =& CCPools::GetTable();
@@ -310,6 +410,7 @@ END;
 
     function Edit($pool_id)
     {
+        require_once('cchost_lib/cc-page.php');
         CCPage::SetTitle(_("Edit Pool Information"));
 
         require_once('cchost_lib/cc-pools-forms.php');
@@ -489,6 +590,7 @@ END;
         CCEvents::MapUrl( ccp( 'pools', 'search' ),    array( 'CCPool', 'Search'),
             CC_DONT_CARE_LOGGED_IN , 'cchost_lib/cc-pools.php' , '{poolid}', 
             _('Search a given pool (perhaps remotely) with a string'), CC_AG_SAMPLE_POOL );
+
         CCEvents::MapUrl( ccp( 'admin', 'pools'),                array( 'CCPoolUI', 'Admin'),    
             CC_ADMIN_ONLY , ccs(__FILE__) , '', _('Display admin pools menu'), CC_AG_SAMPLE_POOL );
         CCEvents::MapUrl( ccp( 'admin', 'pools', 'settings' ),   array( 'CCPoolUI', 'Settings'), 
--- cchost_lib/ccextras/cc-trackback.php
+++ cchost_lib/ccextras/cc-trackback.php
@@ -33,30 +33,31 @@ class CCTrackBack
     function Track($type,$upload_id)
     {
         require_once('cchost_lib/cc-pools.php');
+        require_once('cchost_lib/cc-pools-ui.php');
         $link = $this->_clean_url($_POST['trackback_link']);
-        $pool_id = $this->_get_web_sample_pool();
-        $pool_item_id = 0; // CCDatabase::QueryItem("SELECT pool_item_id FROM cc_tbl_pool_item WHERE pool_item_url = '$link'");
+
+        // http://www.youtube.com/watch?v=9NE3NPytch4
+
+        $xx = CCDatabase::QueryRow("SELECT pool_item_id,pool_item_extra,pool_item_approved FROM cc_tbl_pool_item " . 
+                                    "WHERE pool_item_url = '$link'",false);
+
+        list( $pool_item_id, $existing_extra, $pre_approved ) = $xx;
+
         $pool_items = new CCPoolItems();
         if( empty($pool_item_id) )
         {
-            $new = true;
+            $pool_id        = CCPoolUI::GetWebSamplePool();
             $upload_license = CCDatabase::QueryItem('SELECT upload_license FROM cc_tbl_uploads WHERE upload_id='.$upload_id);
-            $email = CCUTil::Strip($_POST['trackback_email']);
-            $name = CCUTil::Strip($_POST['trackback_your_name']);
+            $email          = CCUTil::Strip($_POST['trackback_email']);
+            $name           = CCUTil::Strip($_POST['trackback_your_name']);
             if( empty($name) )
                 $name = $email;
 
-            $a['pool_item_id'] = $pool_item_id = $pool_items->NextID();
+            $a['pool_item_id']             = $pool_item_id = $pool_items->NextID();
             $a['pool_item_pool']           = $pool_id;
             $a['pool_item_url']            = $link;
             $a['pool_item_download_url']   = '';
             $a['pool_item_description']    = '';
-            $a['pool_item_extra']          = serialize( array( 'ttype'     => $type, 
-                                                               'embed'     => CCUtil::StripSlash($_POST['trackback_media']),
-                                                               'poster'    => $name,
-                                                               'email'     => $email,
-                                                               'upload_id' => $upload_id,
-                                                        ) );
             $a['pool_item_license']        = $upload_license;
             $a['pool_item_name']           = $this->_get_item_name(CCUtil::Strip($_POST['trackback_name']),$link);
             $a['pool_item_artist']         = $this->_get_item_user(CCUtil::Strip($_POST['trackback_artist']),$link);
@@ -64,19 +65,30 @@ class CCTrackBack
             $a['pool_item_timestamp']      = time();
             $a['pool_item_num_remixes']    = 0;
             $a['pool_item_num_sources']    = 0;
+            $a['pool_item_extra']          = serialize( array( 'ttype'     => $type, 
+                                                               'embed'     => CCUtil::StripSlash($_POST['trackback_media']),
+                                                               'poster'    => $name,
+                                                               'email'     => $email,
+                                                               'upload_id' => $upload_id,
+                                                        ) );
 
             $pool_items->Insert($a);
         }
-        $pool_tree = new CCPoolTree();
-        $x['pool_tree_parent'] = $upload_id;
-        $x['pool_tree_pool_child'] = $pool_item_id;
-        $pool_tree->Insert($x);
-        if( empty($new) )
+        else
         {
-            require_once('cchost_lib/cc-sync.php');
-            CCSync::Upload($upload_id);
+            if( $pre_approved )
+            {
+                CCPoolsUI::ApproveTrackback($pool_item_id,$upload_id);
+            }
+            else
+            {
+                $existing_extra = unserialize($existing_extra);
+                $existing_extra['upload_id'] .= ',' . $upload_id;
+                $up['pool_item_extra'] = serialize($existing_extra);
+                $up['pool_item_id'] = $pool_item_id;
+                $pool_items->Update($up);
+            }
         }
-
         print 'ok';
         exit;
     }
@@ -87,9 +99,19 @@ class CCTrackBack
             return $name;
         require_once('cchost_lib/snoopy/Snoopy.class.php');
         $snoopy = new Snoopy();
-        $snoopy->fetch($link);
+        @$snoopy->fetch($link);
         if( !empty($snoopy->error) )
-            $this->_error_out($snoopy->error);
+        {
+            $text1 = _('There was an error trying to validate the web address. Test it in your %sbrowser%s to make sure.');
+            $text1 = sprintf($text1,"<a target=\"_blank\" href=\"$link\">",'</a>');
+            $text2 = _('Gorey details...');
+            $msg =<<<EOF
+{$text1} 
+(<a href="javascript://show error" onclick="$('err_details').style.display='block';return false">{$text2}</a>
+<div style="display:none" id="err_details">{$snoopy->error}</div>
+EOF;
+            $this->_error_out($msg);
+        }
         if( preg_match( '/<meta name="title" content="([^"]+)">/U',$snoopy->results,$m ) )
             return $m[1];
         if( preg_match( '#<title>([^<]+)</title>#',$snoopy->results,$m) )
@@ -126,31 +148,5 @@ class CCTrackBack
         CCEvents::MapUrl( ccp('track'),           array('CCTrackBack', 'Track'),   CC_DONT_CARE_LOGGED_IN, ccs(__FILE__) );
     }
 
-    function _get_web_sample_pool()
-    {
-        $pool_id = CCDatabase::QueryItem('SELECT pool_id FROM cc_tbl_pools WHERE pool_short_name = \'_web\'');
-        if( empty($pool_id) )
-        {
-            require_once('cchost_lib/cc-pools.php');
-            $pools = new CCPools();
-            $a['pool_id'] = $pools->NextID();
-            $a['pool_name'] = _('Trackback Sitings');
-            $a['pool_short_name'] = '_web';
-            $a['pool_description'] = _('People link to us!');
-            // pool_api_url can ba a local module for searching the pool
-            // classname:module_path
-            // CCMagnatune:mixter-lib/mixter-magnatune.inc
-            $a['pool_api_url'] = '';
-            $a['pool_site_url'] = ccl();
-            $a['pool_ip'] = '255.0.0.0';
-            $a['pool_banned'] = 0;
-            $a['pool_search'] = 0;
-            $a['pool_default_license'] = '';
-            $a['pool_auto_approve'] = 0;
-            $pools->Insert($a);
-            $pool_id = $a['pool_id'];
-        }
-        return $pool_id;
-    }
 }
 ?>
