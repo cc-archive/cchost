d67c24b631c44551c0692887a6d771e9a7bf0c60
fourstones <fourstones@committer.creativecommons.org>
2008-03-06 04:30:12 +0000

    titles clipped on remix search_remix

--- ccdataviews/search_remix_artist.php
+++ ccdataviews/search_remix_artist.php
@@ -9,8 +9,13 @@ function search_remix_artist_dataview()
 {
     $sql =<<<EOF
 SELECT 
-    upload_id, user_name, upload_name, user_real_name
-     %columns% 
+    upload_id, user_name, upload_name, 
+    IF( 
+        LOWER(user_real_name) = LOWER(user_name), 
+        user_name, 
+        CONCAT( user_real_name, ' (', user_name, ')' ) 
+       ) as user_real_name
+    %columns% 
 FROM cc_tbl_uploads
 JOIN cc_tbl_user ON upload_user = user_id
 %joins%
