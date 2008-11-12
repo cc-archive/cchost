c7138c2ff6cd35e58528d1568408ad7c4f8b4637
fourstones <fourstones@committer.creativecommons.org>
2008-11-12 00:18:50 +0000

    catch crazyness at mixter

---
+++ mixter-lib/config-catch.php
@@ -0,0 +1,26 @@
+<?
+
+error_reporting(E_ALL);
+
+CCEvents::AddHandler(CC_EVENT_CONFIG_CHAGNED, 'config_changed' );
+
+function config_changed( &$spec, &$old_value )
+{
+    $config_data = CCDatabase::QueryItem("SELECT config_data FROM cc_tbl_config WHERE config_type = 'config'");
+    $config_data = unserialize($config_data);
+    CCDebug::Enable(true);
+    CCDebug::Log("Config changed: type:{$spec['config_type']}");
+    if( $config_data['cc-host-version'] != CC_HOST_VERSION )
+    {
+        CCDebug::Log("CONFIG IS WRONG! Correcting");
+        $config_data['cc-host-version'] = CC_HOST_VERSION;
+        $config_data = serialize($config_data);
+        $table = new CCTable('cc_tbl_config','config_type');
+        $args['config_type'] = 'config';
+        $args['config_data'] = $config_data;
+        $table->Update($args);
+    }
+}
+
+
+?>
