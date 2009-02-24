cb7d12bb1f2f319ae64406a7a65dc992bd0de2f7
fourstones <fourstones@committer.creativecommons.org>
2009-02-24 12:11:16 +0000

    linux likes arial

--- ccskins/shared/colors/font_verdana.php
+++ ccskins/shared/colors/font_verdana.php
@@ -6,6 +6,6 @@
 */?>
 <style type="text/css">
 body, td, input, select, option, ul, h1, h2, h3, h4, p, a, a span {
-  font-family: Verdana, Geneva, Helvetica, sans-serif;
+  font-family: Verdana, Geneva, Arial, sans-serif;
 }
 </style>
