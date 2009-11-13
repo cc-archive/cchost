d646f4e73bdaa2fee8b0878653fca7707930d854
fourstones <fourstones@committer.creativecommons.org>
2009-11-13 19:10:00 +0000

    what it says

---
+++ bin/ccMixter-make_podcast.php
@@ -0,0 +1,29 @@
+<?
+
+
+if( empty($argv[1]) )
+{
+    die( "Missing playlist arg");
+}
+
+$playlist_id = $argv[1];
+
+print( "Playlist id: {$playlist_id}\n" );
+
+require_once( dirname(__FILE__) . '/ccMixter-make_podcast.inc');
+
+
+make_podcast();
+
+function make_podcast()
+{
+    make_podcast_dirs();   
+    convert_to_wavs();
+    build_wav_file();
+    encode_mp3();
+    publish_podcast();
+    post_podcast_topic();
+}
+
+
+?>
