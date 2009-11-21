3e83bb71ee5d5367f4f7fee5c8f707c3c4841db8
fourstones <fourstones@committer.creativecommons.org>
2009-11-21 10:14:26 +0000

    announce outage to community

---
+++ mixter-lib/mixter-schedule-outage.php
@@ -0,0 +1,11 @@
+<?
+
+
+$sched_inc = dirname(__FILE__) . '/mixter-schedule-outage.inc';
+
+CCEvents::AddHandler( CC_EVENT_MAP_URLS,           'schedule_outage_url_map', $sched_inc );
+CCEvents::AddHandler( CC_EVENT_ADMIN_MENU, 'schedule_outage_admin_menu', $sched_inc );
+//CCEvents::AddHandler( CC_EVENT_APP_INIT, 'opt_in_app_init' );
+
+
+?>
