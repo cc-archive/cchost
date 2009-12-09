5041d7ae9dbdc9f0e1710adf5096b4fcdc60bde2
fourstones <fourstones@committer.creativecommons.org>
2009-12-09 03:29:56 +0000

    wrong url in podcast topic

--- bin/ccMixter-post_podcast.php
+++ bin/ccMixter-post_podcast.php
@@ -9,14 +9,14 @@ if( empty($argv[3]) )
     die( "usage: php -f ccMixter-post_podcast.php PLAYLIST_ID URL_TO_MP3 SIZE_IN_MB\n" ) ;
 }
 
-$playlist_id = $argv[1];
-$mp3_url     = $argv[2];
-$size_in_mb  = $argv[3];
+$playlist_id = $argv[1]; // must be before include
 
 print( "Playlist id: {$playlist_id}\n" );
 
 require_once( dirname(__FILE__) . '/ccMixter-make_podcast.inc');
 
+$mp3_url     = $argv[2]; // must be after include
+$size_in_mb  = $argv[3];
 
 make_podcast();
 
