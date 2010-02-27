78bf06bcb2e9d20ec45cba1a1ea124f36e90fd23
fourstones <fourstones@committer.creativecommons.org>
2010-02-27 04:56:58 +0000

    initial checkin

---
+++ dig/lib/footer.php
@@ -0,0 +1,74 @@
+<?
+/*
+* Artistech Media has made the contents of this file
+* available under a CC-GNU-GPL license:
+*
+* http://creativecommons.org/licenses/GPL/2.0/
+*
+* A copy of the full license can be found as part of this
+* distribution in the file LICENSE.TXT.
+* 
+* You may use dig.ccMixter software in accordance with the
+* terms of that license. You agree that you are solely 
+* responsible for your use of dig.ccMixter software and you
+* represent and warrant to Artistech Media that your use
+* of dig.ccMixter software will comply with the CC-GNU-GPL.
+*
+* $Id$
+*
+*/
+?>
+<div id="footer">
+			<div class="footer-column first-footer-column">
+				<h4>Find music for&hellip;</h4>
+				<ul>
+					<li><a href="/podcast_music">&hellip;podcasts</a></li>
+					<li><a href="/music_for_film_and_video">&hellip;videos</a></li>
+					<li><a href="/music_for_games">&hellip;games</a></li>
+					<li><a href="/music_for_entertainment">&hellip;entertainment</a></li>
+				</ul>
+			</div>
+			<div class="footer-column">
+				<h4>About Us</h4>
+				<ul>
+					<li><a href="about.html">dig.ccMixter</a></li>
+					<li><a href="http://ccmixter.org/about">ccMixter</a></li>
+					<li><a href="http://tunetrack.net">TuneTrack</a></li>
+					<li><a href="http://tunetrack.net/artistech/pages/philosophy/">ATM</a></li>
+				</ul>
+			</div>
+			<div class="footer-column">
+				<h4>Blog</h4>
+				<ul>
+					<li><a href="http://ccmixterblog.blogspot.com">All news</a></li>
+					<li><a href="">dig.ccmixter</a></li>
+					<li><a href="">ccMixter</a></li>
+					<li><a href="">TuneTrack</a></li>
+					<li><a href="">ATM</a></li>
+				</ul>
+			</div>
+			<div class="footer-column">
+				<h4>Information</h4>
+				<ul>
+					<li><a href="press-kit.html">Press Kit</a></li>
+					<li><a href="credits.html">Site Credits</a></li>
+					<li><a href="policy.html">Privacy Policy</a></li>
+					<li><a href="tos.html">Terms of Use</a></li>
+				</ul>
+			</div>
+			<div class="footer-column">
+				<h4>Need help?</h4>
+				<ul>
+					<li><a href="faq.html">FAQ</a></li>
+					<li><a href="support.html">Support</a></li>
+				</ul>
+			</div>
+			<div class="footer-column last-footer-column">
+				<h4>Designed by</h4>
+				<ul>
+					<li><a href="http://nvzion.com">nvzion</a></li>
+				</ul>
+			</div>
+			<div class="clearer"></div>
+			<p class="center site-license"><a href="http://creativecommons.org/licenses/by-nc/3.0/us/"><img src="images/by-nc.png" alt="Creative Commons Attribution-Noncommercial 3.0 United States License"></a> This text and images on this site are licensed under a <a href="http://creativecommons.org/licenses/by-nc/3.0/us/">Creative Commons Attribution-Noncommercial 3.0 United States License</a></p>
+		</div>
