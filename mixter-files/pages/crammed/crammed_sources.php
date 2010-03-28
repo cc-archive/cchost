2518b9e4c8748f68735ea129fe4a8eab05161257
fourstones <fourstones@committer.creativecommons.org>
2010-03-28 15:29:08 +0000

    fixed circular links

--- mixter-files/pages/crammed/crammed_sources.php
+++ mixter-files/pages/crammed/crammed_sources.php
@@ -26,20 +26,17 @@ if( !defined('IN_CC_HOST') )
 <h1  class="pagehead">Crammed Discs Remix Sources</h1>
 <div  class="artist">
 <img  src="<?= $T->URL('crammed_small_cibelle.jpg'); ?>" alt="Cibelle" width="280" height="60" />
-<p ><a  href="<?= $A['root-url']?>crammed/files/cibelle/4893">"Noite de Carnaval" (A Capellas)</a> by Cibelle</p>
-<p ><a  href="<?= $A['root-url']?>crammed/files/cibelle/4892">"Noite de Carnaval" (Loops)</a> by Cibelle</p>
+<p ><a  href="<?= $A['root-url']?>files/cibelle/4893">"Noite de Carnaval" (A Capellas)</a> by Cibelle</p>
+<p ><a  href="<?= $A['root-url']?>files/cibelle/4892">"Noite de Carnaval" (Loops)</a> by Cibelle</p>
 </div>
 <div  class="artist">
 <img  src="<?= $T->URL('crammed_small_apollo.jpg'); ?>" alt="Apollo Nove" width="280" height="60" />
-<p ><a  href="<?= $A['root-url']?>crammed/files/apollonove/4904">"Yage Cameras" (Loops)</a> by Apollo Nove</p>
-<p ><a  href="<?= $A['root-url']?>crammed/files/apollonove/4903">"Yage Cameras" (Tracks)</a> by Apollo Nove</p>
+<p ><a  href="<?= $A['root-url']?>files/apollonove/4904">"Yage Cameras" (Loops)</a> by Apollo Nove</p>
+<p ><a  href="<?= $A['root-url']?>files/apollonove/4903">"Yage Cameras" (Tracks)</a> by Apollo Nove</p>
 </div>
 <div  class="artist">
 <img  src="<?= $T->URL('crammed_small_djd.jpg'); ?>" alt="DJ Dolores" width="280" height="60" />
-<p ><a  href="<?= $A['root-url']?>crammed/files/djdolores/4889">"Sanidade" (A Capellas)</a> by DJ Dolores</p>
-<p ><a  href="<?= $A['root-url']?>crammed/files/djdolores/4888">"Sanidade" (Loops)</a> by DJ Dolores</p>
+<p ><a  href="<?= $A['root-url']?>files/djdolores/4889">"Sanidade" (A Capellas)</a> by DJ Dolores</p>
+<p ><a  href="<?= $A['root-url']?>files/djdolores/4888">"Sanidade" (Loops)</a> by DJ Dolores</p>
 </div>
-<p >
-	You can listen to the original versions of the songs by visiting <a  href="http://www.crammed.be/news/cc.htm">the Crammed Discs Web site</a> (RealPlayer required).
-</p>
 </div>
